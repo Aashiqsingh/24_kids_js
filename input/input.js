c3bdfb9825de42814b578d63b50a6cc24ce8f4e0
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-01 18:43:33 +0530

    add

--- input/input.js
+++ input/input.js
@@ -4,6 +4,28 @@
 // console.log(ans);
 
 
-var names = prompt("Enter your name :")
-console.log(names);
+// var names = prompt("Enter your name :")
+// console.log(names);
+
+// var a = parseInt(prompt("Enter first number :"));
+// var b = parseInt(prompt("Enter second number :"));
+
+// console.log(a+b);
+
+// "8" + "10"
+
+
+// var a = 10;
+
+// console.log("value of a = ",a);
+// console.log("value of a = "+ a);
+
+
+var a = parseInt(prompt("Enter first number :-"))
+var b = parseInt(prompt("Enter second number :-"))
+
+console.log("Addition = ",a+b);
+
+
+
 
