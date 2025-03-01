6d0906a09705e88246a0b5c816bd4fa7554083db
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-01 17:50:56 +0530

    input

---
+++ input/input.js
@@ -0,0 +1,9 @@
+// alert("Hello good morning all....")
+
+// var ans = confirm("Are you married?")
+// console.log(ans);
+
+
+var names = prompt("Enter your name :")
+console.log(names);
+
---
+++ operator/Arithmatic.js
@@ -0,0 +1,21 @@
+var a = 20;
+var b = 12;
+
+
+let add = a + b;
+console.log("Addition = ",add);
+
+
+let sub = a - b;
+console.log("Subtraction = ", sub);
+
+
+let multiply = a * b;
+console.log("Multiplication = ", multiply);
+
+
+let divide = a / b;
+console.log("Divide = ",divide);
+
+let module = a % b;
+console.log("Modulo = ", module);
