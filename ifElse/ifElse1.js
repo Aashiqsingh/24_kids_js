1e9f68e433fe55fb4efbba9e723ccf05ceb087b2
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-08 17:15:57 +0530

    ifElse

---
+++ ifElse/ifElse1.js
@@ -0,0 +1,66 @@
+// 1 . if
+// 2. if else 
+// 3. ladder if else 
+// 4. nested if else
+
+// var num = -19;
+
+// if(num > 0)
+// {
+//     console.log("Number is positive");
+// }
+
+
+// if else ....
+
+// var num = 0;
+
+// if(num > 0)
+// {
+//     console.log("Number is positive");
+// }
+// else{
+//     console.log("Number is negative");
+    
+// }
+
+
+// nested if else 
+
+// var num = -1;
+
+// if(num > 0)
+// {
+//     console.log("Number is Positive");
+    
+// }
+// else if(num == 0)
+// {
+//     console.log("Number is Zero");
+// }
+// else{
+//     console.log("Number is Negative");
+// }
+
+
+// nested if Else
+
+var isAlive = true;
+var age = 16;
+
+if(isAlive == true)
+{
+    console.log("The man is alive..");
+    if(age > 18)
+    {
+        console.log("He is adult");
+    }
+    else{
+        console.log("He is child");
+    }
+    
+}   
+else{
+    console.log("DEAD");
+    
+} 
