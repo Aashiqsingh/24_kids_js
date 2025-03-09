fcf38731f6479d72244285e700117b269e8eabe6
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-09 15:44:50 +0530

    ternary

---
+++ ternary/ternary1.js
@@ -0,0 +1,28 @@
+var a = 13;
+var b = 14;
+
+
+// var x = a > b ? a : b;
+// console.log(x + " is gretter");
+
+
+
+
+// a < b ? console.log("b is bigger") : console.log("a is bigger");
+
+
+
+// var ans = a > b ? "a is bigger" : "b is bigger";
+
+// console.log(ans);
+
+
+
+// if(a > b)
+// {
+//     console.log(a + " is greater");
+    
+// }
+// else{
+//     console.log(b + " is greater");
+// }
