30727b1207523848f54a711d8328feb1998c9300
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-09 16:15:32 +0530

    .

--- ternary/ternary1.js
+++ ternary/ternary1.js
@@ -1,5 +1,5 @@
-var a = 13;
-var b = 14;
+// var a = 13;
+// var b = 14;
 
 
 // var x = a > b ? a : b;
@@ -26,3 +26,60 @@ var b = 14;
 // else{
 //     console.log(b + " is greater");
 // }
+
+// var num = 16
+
+// var x = num % 2 == 0 ? "Even" : "Odd";
+// console.log(x);
+
+var a = 19;
+var b = 18;
+var c = 17;
+
+
+
+
+
+
+
+// if(a > b && a > c)
+// {
+//     console.log(a + " is the biggest");
+// }
+// else if(b > a && b > c)
+// {
+//     console.log(b + " is the biggest");
+// }
+// else{
+//     console.log(c + " is the biggest");
+// }
+
+
+
+
+
+
+
+
+
+
+// if(a > b)
+// {
+//     if(a > c)
+//     {
+//         console.log(a + " is the biggest");
+//     }
+//     else{
+//         console.log(c + " is the biggest");
+//     }
+// }
+// else{
+//     if(b > c)
+//     {
+//         console.log(b + " is the biggest");
+//     }
+//     else{
+//         console.log(c + " is the biggest");
+//     }
+// }
+
