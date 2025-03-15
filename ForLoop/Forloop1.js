e6adcb8c4654312f321c74bbebacf71054bba04f
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-15 17:18:36 +0530

    loop

--- ForLoop/Forloop1.js
+++ ForLoop/Forloop1.js
@@ -1,3 +1,48 @@
+// revrese number :
+
+// var num = 1345;
+// var rev = 0;
+
+// for(;num > 0;)
+// {
+//     var rem = num % 10;
+//     rev = rev * 10 + rem
+//     num = Math.floor(num / 10);
+// }
+
+// console.log(rev);
+
+
+// fibbonacci number 
+// 0 1 1 2 3 5 8 13 21 34
+
+let a = 0;
+let b = 1;
+console.log(a);
+console.log(b);
+
+for(let i=0;i<10;i++)
+{
+    let c = a + b;
+    console.log(c);
+    a = b;
+    b = c;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // for(let i=0;i<5;i++)
@@ -10,21 +55,21 @@
 
 // 
 
-var num = 18;
-var flag = true;
+// var num = 18;
+// var flag = true;
 
-for(let i=2;i<num;i++)
-{
-    if(num % i == 0)
-    {
-        flag = false;
-    }
-}
+// for(let i=2;i<num;i++)
+// {
+//     if(num % i == 0)
+//     {
+//         flag = false;
+//     }
+// }
 
-if(flag === true)
-{
-    console.log("The number is prime");
-}
-else{
-    console.log("The number is not prime");
-}
+// if(flag === true)
+// {
+//     console.log("The number is prime");
+// }
+// else{
+//     console.log("The number is not prime");
+// }
