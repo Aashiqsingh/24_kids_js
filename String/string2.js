5251838debf3cbc13035bd01262599105eafcd1c
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-29 17:38:27 +0530

    string method

---
+++ String/string2.js
@@ -0,0 +1,38 @@
+// var str = "Aditya Satva Rihaan";
+
+
+// console.log(str);
+
+// var x = str.includes("a")
+// console.log(x);
+
+// var x = str.indexOf("a")
+// console.log(x);
+
+// var x = str.repeat(3)
+// console.log(x);
+
+// var x = str.replace("t","z")
+// var x = str.replaceAll("t","z")
+
+// var x = str.replace("Ri","MEET")
+
+// console.log(x);
+
+var str = "Royal technosoft pvt ltd.";
+
+// var x = str.slice(1)
+// var x = str.slice(1,4)
+// var x = str.slice(-4)
+// var x = str.slice(-4,-1)
+
+
+// console.log(x);
+
+
+// var x = str.split(" ")
+// var x = str.split("o")
+// var x = str.split("")
+
+
+// console.log(x);
