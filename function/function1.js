0f789478a961a4048bd728d63cc6a9860da1f8cd
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-30 15:18:57 +0530

    function

---
+++ function/function1.js
@@ -0,0 +1,45 @@
+// without returntype without arguments
+function demo()
+{
+    console.log("demo function called...");
+    
+}
+
+// demo()
+
+// without returntype with arguments
+
+function addition(a,b)
+{
+    console.log("Addition =",a+b);
+    
+}
+
+
+// addition(10,20)
+// addition()
+
+// with returntype without arguments
+
+function test()
+{
+    return "test function called..."
+}
+
+// let x = test()
+// console.log(x);
+
+// console.log(test());
+
+
+// with returntype with arguments
+
+function multiply(x,y,z){
+    return x*y*z;
+}
+
+var ans = multiply(2,3,4)
+console.log("multiply = ",ans);
+
+
+
