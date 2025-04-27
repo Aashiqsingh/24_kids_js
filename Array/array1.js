fd0047c77134a6b3227852bf8a3402fb9b50ef12
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-04-27 16:00:56 +0530

    array

---
+++ Array/array1.js
@@ -0,0 +1,67 @@
+// var arr = [12 , 15.55 , true , "ram" , "hanuman" , 134];
+
+
+// console.log(arr);
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+// console.log(arr.length);
+
+
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+    
+// }
+
+
+var users = ["madhav","heer","ranjha","samay","harsh","rahul","rajvi","priya"];
+
+// var newArray = []
+
+
+// for(let i=0;i<users.length;i++)
+// {
+//     // console.log(users[i]);
+//     newArray.push(users[i].toUpperCase())
+// }
+// console.log(newArray);
+
+// console.log(users);
+
+// users.pop()
+
+// console.log(users);
+
+
+
+
+
+// console.log(users);
+// for(let i=0;i<users.length;i++)
+// {
+//     console.log(users[i].toUpperCase());
+    
+// }
+// console.log(users);
+
+// // users.push("shubham")
+// var x = users.push("shubham","guru")
+
+
+// console.log(users);
+// console.log(x);
+
+console.log(users);
+
+
+var x = users.unshift("moksh","patel")
+// console.log(x);
+console.log(users);
+
+users.shift()
+
+console.log(users);
+
+
