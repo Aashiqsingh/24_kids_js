6623eb1d8704740cfd87e749ed3d5d1faacc8721
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-10 17:57:23 +0530

    dom

---
+++ Dom/dom1.js
@@ -0,0 +1,21 @@
+function changeTxt(){
+    let txt = document.getElementById("txt")
+
+    txt.innerHTML = "Royal technosoft p ltd."
+    txt.style.backgroundColor = "red";
+    txt.style.color = "white";
+
+}
+
+function changeRef(){
+    let link = document.getElementById("link")
+
+    link.href = "https://netflix.com";
+    link.innerHTML = "Netflix"
+}
+
+function changeSize(){
+    let image = document.getElementById("image")
+    image.style.height = "400px";
+    image.style.width = "500px"
+}
