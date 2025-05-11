2d9c6fbe4b1e2049da7c32a4665c102efc726453
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-11 16:30:05 +0530

    onclick

---
+++ Dom/dom2.js
@@ -0,0 +1,32 @@
+var mydiv = document.getElementById("mydiv")
+function red(){
+    mydiv.style.height = "200px";
+    mydiv.style.width = "200px";
+    mydiv.style.backgroundColor = "red"
+}
+
+function green(){
+    mydiv.style.height = "200px";
+    mydiv.style.width = "200px";
+    mydiv.style.backgroundColor = "green"
+}
+function blue(){
+    mydiv.style.height = "200px";
+    mydiv.style.width = "200px";
+    mydiv.style.backgroundColor = "blue"
+}
+function pink(){
+    mydiv.style.height = "200px";
+    mydiv.style.width = "200px";
+    mydiv.style.backgroundColor = "pink"
+}
+function yellow(){
+    mydiv.style.height = "200px";
+    mydiv.style.width = "200px";
+    mydiv.style.backgroundColor = "yellow"
+}
+function orange(){
+    mydiv.style.height = "200px";
+    mydiv.style.width = "200px";
+    mydiv.style.backgroundColor = "orange"
+}
